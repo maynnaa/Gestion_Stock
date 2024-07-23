a50ee2f38edfc41cb1091eb8e1a75a2e6a25a03a
maynnaa <134529914+maynnaa@users.noreply.github.com>
2024-07-23 10:56:57 +0100

    cc

--- Gestion_Stock/gestion_stock_frontend/src/App.js
+++ Gestion_Stock/gestion_stock_frontend/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import PageWithSidebar from './pages/sidebar';
+import LoginPage from './pages/login';
 
 function App() {
   return (
     <div className="App">
+      <LoginPage />
       <PageWithSidebar />
+      
     </div>
   );
 }
 
-export default App;
+export default App;
