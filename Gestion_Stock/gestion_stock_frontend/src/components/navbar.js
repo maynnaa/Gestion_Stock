ef11e1ce930e387e1e03a60bf473a46ab5f023e0
hanaeray <134614305+hanaeray@users.noreply.github.com>
2024-08-09 12:14:40 +0100

    notification

--- Gestion_Stock/gestion_stock_frontend/src/components/navbar.js
+++ Gestion_Stock/gestion_stock_frontend/src/components/navbar.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Navbar, Nav, Dropdown } from 'react-bootstrap';
 import { FaBell, FaSignOutAlt } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 
 const NavBar = ({ children }) => {
   const navigate = useNavigate();
+  const [showNotifications, setShowNotifications] = useState(false);
 
   const handleNavigation = (path) => {
     navigate(path);
@@ -41,15 +42,57 @@ const NavBar = ({ children }) => {
             align-items: center;
             margin-right: 30px;
           }
+
+          .dropdown-menu {
+            width: 300px;
+          }
+
+          .dropdown-item {
+            display: flex;
+            align-items: center;
+            white-space: normal;
+            padding: 10px;
+          }
+
+          .dropdown-item:hover {
+            background-color: #f0f0f0;
+          }
+
+          .notification-title {
+            font-weight: bold;
+            margin-bottom: 5px;
+          }
+
+          .notification-text {
+            color: #555;
+          }
         `}
       </style>
       <Navbar className="navbar-custom" expand="lg">
         <Nav className="navbar-nav">
           {children}
           <Nav className="nav-right">
-            <Nav.Link href="#notifications">
-              <FaBell size={25} />
-            </Nav.Link>
+            <Dropdown show={showNotifications} onToggle={(isOpen) => setShowNotifications(isOpen)}>
+              <Dropdown.Toggle as="a" className="nav-link" id="notification-dropdown">
+                <FaBell size={25} />
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu align="end">
+                <Dropdown.Item>
+                  <div className="notification-title">New Comment</div>
+                  <div className="notification-text">Someone commented on your post.</div>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  <div className="notification-title">New Like</div>
+                  <div className="notification-text">Someone liked your post.</div>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  <div className="notification-title">New Message</div>
+                  <div className="notification-text">You have received a new message.</div>
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+
             <Nav.Link href="#" onClick={() => handleNavigation('/login')}>
               <FaSignOutAlt size={25} />
             </Nav.Link>
